990599014c794456dfde30f9ad2ea760c31150c3
HB <h.belouali@cosumar.ma>
2025-02-17 22:51:37 +0100

    update entrypoint to create user

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,19 @@ echo "Applying migrations..."
 python manage.py migrate
 
 # Create superuser if DJANGO_SUPERUSER_USERNAME is set
-if [ -n "${DJANGO_SUPERUSER_USERNAME}" ]; then
+if [ -n "${DJANGO_SUPERUSER_USERNAME}" ] && [ -n "${DJANGO_SUPERUSER_PASSWORD}" ]; then
     echo "Creating superuser..."
-    python manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_EMAIL \
-        --password $DJANGO_SUPERUSER_PASSWORD
+    python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='${DJANGO_SUPERUSER_USERNAME}').exists():
+    User.objects.create_superuser('${DJANGO_SUPERUSER_USERNAME}', 
+                                '${DJANGO_SUPERUSER_EMAIL}', 
+                                '${DJANGO_SUPERUSER_PASSWORD}')
+    print('Superuser created.')
+else:
+    print('Superuser already exists.')
+END
 fi
 
 # Start the application
